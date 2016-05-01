c30cba6f586bf9f36450d40b8e8fa199c460d00f
Matt Ranney <mranney@uber.com>
2016-05-01 02:51:46 -0700

    Added user and pid filters, removed some accidentally included other files.

---
+++ lib/filters.go
@@ -0,0 +1,105 @@
+package cpustat
+
+import (
+	"log"
+	"os/user"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+var splitter = regexp.MustCompile("[, ] *")
+
+// ParseUsrList take a string of Unix usernames and converts it into a slice of int userids
+func ParseUserList(s string) ([]int, error) {
+	parts := splitter.Split(s, -1)
+	ret := make([]int, len(parts))
+	for pos, part := range parts {
+		userEnt, err := user.Lookup(part)
+		if err != nil {
+			return nil, err
+		}
+
+		uid, err := strconv.Atoi(userEnt.Uid)
+		if err != nil {
+			return nil, err
+		}
+		ret[pos] = uid
+	}
+	return ret, nil
+}
+
+// ParsePidList take a string of process ids and converts it into a list of int pids
+func ParsePidList(s string) ([]int, error) {
+	parts := splitter.Split(s, -1)
+	ret := make([]int, len(parts))
+	for pos, part := range parts {
+		if len(part) == 0 {
+			continue
+		}
+		num, err := strconv.Atoi(part)
+		if err != nil {
+			panic(err)
+		}
+		ret[pos] = num
+	}
+	return ret, nil
+}
+
+type Filters struct {
+	User []int
+	Pid  []int
+}
+
+func (f Filters) PidMatch(pid int) bool {
+	if len(f.Pid) == 0 {
+		return true
+	}
+
+	pos := sort.SearchInts(f.Pid, pid)
+	if pos == len(f.Pid) {
+		return false
+	}
+	if f.Pid[pos] == pid {
+		return true
+	}
+	return false
+}
+
+func (f Filters) UserMatch(user int) bool {
+	if len(f.User) == 0 {
+		return true
+	}
+
+	pos := sort.SearchInts(f.User, user)
+	if pos == len(f.User) {
+		return false
+	}
+	if f.User[pos] == user {
+		return true
+	}
+	return false
+}
+
+func FiltersInit(user, pid string) Filters {
+	ret := Filters{}
+
+	var err error
+	if user != "" {
+		ret.User, err = ParseUserList(user)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sort.Ints(ret.User)
+	}
+
+	if pid != "" {
+		ret.Pid, err = ParsePidList(pid)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sort.Ints(ret.Pid)
+	}
+
+	return ret
+}
